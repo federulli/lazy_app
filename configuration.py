840d237ed6dc09c0a6db98abd67a45db2069a8ec
Federico Rulli <fede.rulli@gmail.com>
2019-07-09 15:16:06 -0300

    fix bug

--- configuration.py
+++ configuration.py
@@ -1,33 +1,39 @@
-from video.models import Configuration as db_config
+from models import Configuration as db_config
 import os
 
 
-class Configuration(object):
+class Configuration:
 
-    def __init__(self):
+    """def __init__(self):
         self._value_by_name = {
             config.name: config.value
             for config in db_config.objects.all()
-        }
+        }"""
 
     @property
     def yts_url(self):
-        return self._value_by_name['yts_url']
+        return 'https://yts.am/api/v2'
+        #return self._value_by_name['yts_url']
 
     @property
     def tpb_url(self):
-        return self._value_by_name['tpb_url']
+        pass
+        #return self._value_by_name['tpb_url']
 
     @property
     def base_download_path(self):
-        return self._value_by_name['base_download_path']
+        return "/tmp/"
+        #return self._value_by_name['base_download_path']
 
     @property
     def movie_download_path(self):
-        return os.path.join(self.base_download_path,
-                            self._value_by_name['movies_sub_path'])
+        return "/tmp/"
+        """return os.path.join(self.base_download_path,
+                            self._value_by_name['movies_sub_path'])"""
 
     @property
     def tv_show_download_path(self):
-        return os.path.join(self.base_download_path,
+        return "/tmp/"
+        """return os.path.join(self.base_download_path,
                             self._value_by_name['tv_shows_sub_path'])
+        """
